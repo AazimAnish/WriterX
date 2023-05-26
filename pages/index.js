4435979a596667300fa107e93efaf74ae1408e21
AazimAnish <aazimanish1@gmail.com>
2023-05-26 15:36:48 +0530

    API key setup

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,29 @@ import { useState } from 'react';
 const Home = () => {
   const [userInput, setUserInput] = useState('');
 
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
@@ -16,13 +39,12 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>AI-powered DSA problem solver.</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Provide a DSA problem statement or algorithm you need help with, and let the AI guide you towards a solution.</h2>
           </div>
         </div>
-        {/* Add this code here*/}
         <div className="prompt-container">
           <textarea
             className="prompt-box"
@@ -30,6 +52,14 @@ const Home = () => {
             value={userInput}
             onChange={onUserChangedText}
           />
+          {/* New code added here */}
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       <div className="badge-container grow">
