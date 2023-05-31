75b63b648d2e22b0da99216eb6cdc4d4bbafcc37
AazimAnish <aazimanish1@gmail.com>
2023-05-31 22:32:11 +0530

    added base promt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,14 +6,14 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+const basePromptPrefix = "Write the fastest, most memory-efficient, and simplest solution to solve the DSA problem:-";
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.7,
     max_tokens: 250,
   });
